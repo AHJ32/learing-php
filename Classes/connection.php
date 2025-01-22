1c59dbcd7ecadb2aeb381989993416b683e8f3c8
Anamul hauqe <70460758+AHJ32@users.noreply.github.com>
2025-01-22 10:58:24 +0600

    Learned how to insert data

---
+++ Classes/connection.php
@@ -0,0 +1,9 @@
+<!-- Connecting to the DB -->
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$dbname = "anamul";
+
+$con = mysqli_connect($server, $username, $password, $dbname);
+?>
--- Classes/index.php
+++ Classes/index.php
@@ -1,13 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Insertion</title>
+</head>
+<body>
+    <main>
+        <form method="POST">
+            <p>Roll:</p>
+            <input type="number" name = "roll">
+            <p>Reg. No:</p>
+            <input type="number" name = "reg">
+            <p>Email:</p>
+            <input type="text" name = "email"><br><br>
+            <button name ="btn">Submit </button>
+        </form>
+    </main>
+</body>
+</html>
+
 <?php
+    include "connection.php";
+    if (isset($_POST["btn"])) {
+        $roll = $_POST["roll"];
+        $reg = $_POST["reg"];
+        $email = $_POST["email"];
 
-
-// $count = 0;
-// while ($count <= 10) {
-//     echo $count;
-//     $count++;
-// }
-for ($i = 0; $i < 11; $i++) {
-    echo $i;
-}
-
+        $query_insertion = "INSERT INTO `student_details`(`roll`, `reg. no.`, `email`) VALUES ('$roll', '$reg' ,'$email')";
+         $execute = mysqli_query( $con, $query_insertion); 
+    }
 ?>
